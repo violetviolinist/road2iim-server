5b22d1950dd5d58a8245257be437bc10dbad2236
Jay Parekh <jay.parekh0@gmail.com>
2024-08-30 22:35:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/road2iim/webhooks', (req, res) => {
+    const eventData = req.body;
+    
+    if (eventData && eventData.event === 'payment.captured') {
+      if (eventData && eventData.payload && eventData.payload.payment && eventData.payload.payment.entity) {
+        const email = eventData.payload.payment.entity.email;
+        console.log('Email from webhook:', email);
+        res.sendStatus(200);
+        return
+      } else {
+          console.log('Invalid webhook data structure', eventData);
+      }
+    }
+
+    res.sendStatus(500);
+});
+
+const PORT = 8082;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
